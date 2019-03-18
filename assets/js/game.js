dc5f2778d934583559d5beac2d0b8267707a0e90
GunnyD <45931516+DeanJG@users.noreply.github.com>
2019-03-18 14:29:51 -0700

    js timer functionality added

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,19 @@
 let numRight,
     numWrong,
     numUnAns,
-    count
+    count = 45;
+
+
+const countdown = _ => {
+    count--
+    count === -1 ? clearInterval(gameCountdown) : document.querySelector(`#gameTimer`).innerHTML = `Time Remaining: ${count} Seconds`
+
+}
+const gameEnd = _ => {
+    alert(`Time's Up!`)
+}
+
+const gameCountdown = setInterval(countdown, 1000)
+const gameTime = setTimeout(gameEnd, 46000)
+
+
