203b27b3e6c505e2c2431e74bfcfe5d3176d6e38
GunnyD <45931516+DeanJG@users.noreply.github.com>
2019-03-18 16:27:55 -0700

    added js to check and tally responses

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,10 @@
-let numRight,
-    numWrong,
-    numUnAns,
+let numRight = 0,
+    numWrong = 0,
+    numUnans = 10,
+    endGameMsg,
+    chosenAnswers = [],
+    rightAns = [],
+    wrongAns = [],
     count = 45;
 
 
@@ -9,9 +13,40 @@ const countdown = _ => {
     count === -1 ? clearInterval(gameCountdown) : document.querySelector(`#gameTimer`).innerHTML = `Time Remaining: ${count} Seconds`
 
 }
-const gameEnd = _ => {
-    alert(`Time's Up!`)
+
+const answerTotals = _ => {
+    chosenAnswers.push(document.querySelector('input[name=qOneAns]:checked').value)
+    chosenAnswers.push(document.querySelector('input[name=qTwoAns]:checked').value)
+    chosenAnswers.push(document.querySelector('input[name=qThreeAns]:checked').value)
+    chosenAnswers.push(document.querySelector('input[name=qFourAns]:checked').value)
+    chosenAnswers.push(document.querySelector('input[name=qFiveAns]:checked').value)
+    chosenAnswers.push(document.querySelector('input[name=qSixAns]:checked').value)
+    chosenAnswers.push(document.querySelector('input[name=qSevenAns]:checked').value)
+    chosenAnswers.push(document.querySelector('input[name=qEightAns]:checked').value)
+    chosenAnswers.push(document.querySelector('input[name=qNineAns]:checked').value)
+    chosenAnswers.push(document.querySelector('input[name=qTenAns]:checked').value)
+    for (let i = 0; i < chosenAnswers.length; i++) {
+        if (chosenAnswers[i] === `right`) {
+            numRight++
+        } else if (chosenAnswers[i] === `wrong`) {
+            numWrong++
+        }
+    }
+    console.log(chosenAnswers)
+    console.log(numRight)
+    console.log(numWrong)
+    console.log(numUnans -= (numRight + numWrong))
 }
+
+const gameEnd = _ => {
+    clearTimeout(gameTime)
+    alert(`Time's Up!`)
+    answerTotals()
+}
+
+// grabbing chosen answer value for one question
+// document.querySelector('input[name=qOneAns]:checked').value
+
 
 const gameCountdown = setInterval(countdown, 1000)
 const gameTime = setTimeout(gameEnd, 46000)
